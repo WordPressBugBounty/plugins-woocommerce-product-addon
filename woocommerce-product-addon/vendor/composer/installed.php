5d3903094b124d9254257e009e9ebb7fc62013ea
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 16:44:09 +0000

    32.0.27

---
+++ woocommerce-product-addon/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/woocommerce-product-addon',
+        'pretty_version' => 'v32.0.27',
+        'version' => '32.0.27.0',
+        'reference' => '5c14b9ecce897fcd2664c3ef3c4a40c98ede2d5d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.28',
+            'version' => '3.3.28.0',
+            'reference' => 'cd35fd5356ba169b9a0419ba879764ffeb811b63',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/woocommerce-product-addon' => array(
+            'pretty_version' => 'v32.0.27',
+            'version' => '32.0.27.0',
+            'reference' => '5c14b9ecce897fcd2664c3ef3c4a40c98ede2d5d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
