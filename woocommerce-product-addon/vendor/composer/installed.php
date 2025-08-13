3ebc409f847c2685d618e7e3778978f695701644
Sean Murphy <sean@iamseanmurphy.com>
2025-08-13 13:25:14 +0000

    33.0.14

--- woocommerce-product-addon/vendor/composer/installed.php
+++ woocommerce-product-addon/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/woocommerce-product-addon',
-        'pretty_version' => 'v33.0.13',
-        'version' => '33.0.13.0',
-        'reference' => 'ed18a86b622f246ad884050b60688d2236a6013c',
+        'pretty_version' => 'v33.0.14',
+        'version' => '33.0.14.0',
+        'reference' => 'e90315a50bd812ac19ea30c07b15575240cfc8a4',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.46',
-            'version' => '3.3.46.0',
-            'reference' => 'af2e7360bbe3af6a1bd2c3779fc33b7765104461',
+            'pretty_version' => '3.3.47',
+            'version' => '3.3.47.0',
+            'reference' => '59499ba103c0369c98b9e0d878826939e3e8e408',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/woocommerce-product-addon' => array(
-            'pretty_version' => 'v33.0.13',
-            'version' => '33.0.13.0',
-            'reference' => 'ed18a86b622f246ad884050b60688d2236a6013c',
+            'pretty_version' => 'v33.0.14',
+            'version' => '33.0.14.0',
+            'reference' => 'e90315a50bd812ac19ea30c07b15575240cfc8a4',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
