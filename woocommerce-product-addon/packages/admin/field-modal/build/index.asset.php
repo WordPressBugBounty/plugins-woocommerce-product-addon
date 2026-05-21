ba67e6a4cf18eeac1e6f1d87c0ca706b84264561
Sean Murphy <sean@iamseanmurphy.com>
2026-05-21 07:35:47 +0000

    34.0.1

--- woocommerce-product-addon/packages/admin/field-modal/build/index.asset.php
+++ woocommerce-product-addon/packages/admin/field-modal/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '5faf00415b8d0a408d67');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '25a222bcee863570da48');
--- woocommerce-product-addon/vendor/composer/installed.php
+++ woocommerce-product-addon/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/woocommerce-product-addon',
-        'pretty_version' => 'v34.0.0',
-        'version' => '34.0.0.0',
-        'reference' => '7adbc671d5b259acb1eced38d9f6ce92d200ae9a',
+        'pretty_version' => 'v34.0.1',
+        'version' => '34.0.1.0',
+        'reference' => '6d7741b1d4dd5e1fbc3b74b5e2706ff83619d4b6',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.51',
-            'version' => '3.3.51.0',
-            'reference' => 'bb2a8414b0418b18c68c9ff1df3d7fb10467928d',
+            'pretty_version' => '3.3.52',
+            'version' => '3.3.52.0',
+            'reference' => 'd1ae68cbd4f84934b4d982e9eeff317b9f4c814a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/woocommerce-product-addon' => array(
-            'pretty_version' => 'v34.0.0',
-            'version' => '34.0.0.0',
-            'reference' => '7adbc671d5b259acb1eced38d9f6ce92d200ae9a',
+            'pretty_version' => 'v34.0.1',
+            'version' => '34.0.1.0',
+            'reference' => '6d7741b1d4dd5e1fbc3b74b5e2706ff83619d4b6',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
