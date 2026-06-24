60d48d24d7e52f10ac4f1648c18945d2489fd15b
Sean Murphy <sean@iamseanmurphy.com>
2026-06-24 13:56:14 +0000

    34.0.4

--- woocommerce-product-addon/vendor/composer/installed.php
+++ woocommerce-product-addon/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/woocommerce-product-addon',
-        'pretty_version' => 'v34.0.3',
-        'version' => '34.0.3.0',
-        'reference' => '19fa1ac36b444116ef3c9585c0319d07bcfb5af2',
+        'pretty_version' => 'v34.0.4',
+        'version' => '34.0.4.0',
+        'reference' => '8be1b21c1504fe9113862e406bae46384debd17e',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'codeinwp/woocommerce-product-addon' => array(
-            'pretty_version' => 'v34.0.3',
-            'version' => '34.0.3.0',
-            'reference' => '19fa1ac36b444116ef3c9585c0319d07bcfb5af2',
+            'pretty_version' => 'v34.0.4',
+            'version' => '34.0.4.0',
+            'reference' => '8be1b21c1504fe9113862e406bae46384debd17e',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
